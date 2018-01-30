4591d5b9b7784f61f2b54e7aff6cba9087d6ace2
yana <yana.biliaieva@gmail.com>
2018-01-30 03:35:54 +0200

    initial

---
+++ src/main/java/authentification/Authentication.java
@@ -0,0 +1,24 @@
+package authentification;
+
+import model.entities.User;
+import org.apache.log4j.Logger;
+
+import javax.servlet.http.HttpSession;
+import java.util.Objects;
+
+public class Authentication {
+    private static Logger logger = Logger.getLogger(Authentication.class);
+    private Authentication() {
+    }
+
+    public static boolean isUserLogIn(HttpSession session){
+        User user = (User) session.getAttribute("user");
+        logger.info("Authentication isUserLogIn");
+        return Objects.nonNull(user);
+    }
+    public static boolean isAdmin(HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        logger.info("Authentication isAdmin");
+        return Objects.nonNull(user) && Objects.nonNull(user.getRole()) && user.getRole().equals("ADMIN");
+    }
+}
